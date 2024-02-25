9cdb8e98dbbb81e8fc080ab22e912697441749ea
JustinSms <justinsams98@yahoo.de>
2024-02-25 14:32:57 +0000

    method3 start

--- phase3/flight_analyzer.py
+++ phase3/flight_analyzer.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
+# Download the data
+airlines_df = pd.read_csv("downloads/airlines.csv")
+airplanes_df = pd.read_csv("downloads/airplanes.csv")
+airports_df = pd.read_csv("downloads/airports.csv")
+routes_df = pd.read_csv("downloads/routes.csv")
 
 class FlightAnalyzer():
 
@@ -13,8 +18,20 @@ class FlightAnalyzer():
     def method1(self):
         pass
 
-    def method2(self):
-        pass    
+    def method2(self, airport, internal=False):
+        """Develop a third method that receives an airport as an input and an optional argument called internal 
+        with a value of False by default. If internal is True, then this method should plot only the flights 
+        leaving this airport with a destination in the same country. Otherwise, it plots all flights."""
+
+        if internal == False:
+            all_routes = self.routes_df[self.routes_df['Source airport'] == airport]
+            print(all_routes)
+
+        if internal == True:
+            internal
+            
+            #print(internal_routes)
+           
 
     def method3(self):
         pass
@@ -25,4 +42,7 @@ class FlightAnalyzer():
     def method5(self): 
         pass
 
-    
+
+
+FA = FlightAnalyzer(airlines_df, airplanes_df, airports_df, routes_df)
+FA.method2('MUC')
