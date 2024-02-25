1dcf1c3095b8210d5b6a6c6cae80cad82e59aae9
JustinSms <justinsams98@yahoo.de>
2024-02-25 13:56:29 +0000

    class to import into notebook

---
+++ phase3/flight_analyzer.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+class FlightAnalyzer():
+
+    def __init__ (self, airlines_df, airplanes_df, airports_df, routes_df):
+        self.airlines_df = airlines_df
+        self.airplanes_df = airplanes_df
+        self.airports_df = airports_df
+        self.routes_df = routes_df
+
+
+    def method1(self):
+        pass
+
+    def method2(self):
+        pass    
+
+    def method3(self):
+        pass
+
+    def method4(self):
+        pass    
+
+    def method5(self): 
+        pass
+
+    
