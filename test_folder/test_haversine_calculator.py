17d65a0ba2ee9b997874b07a2afd5ee56b1a2cd0
JustinSms <justinsams98@yahoo.de>
2024-03-16 22:28:55 +0000

    Correct Airport to airport requirement and update the readme

--- test_folder/test_haversine_calculator.py
+++ test_folder/test_haversine_calculator.py
@@ -19,7 +19,7 @@ def test_haversine_distance():
         0, rel=1e-4)
     # Berlin to Munich
     assert haversine_distance(52.5200, 13.4050, 48.1351, 11.5820) == pytest.approx(
-        504.3, rel=1e-3)
-    # Los Angeles to New York
-    assert haversine_distance(34.0522, -118.2437, 40.7128, -74.0060) == pytest.approx(
-        3933.1, rel=1e-3)
+        504.3, rel=1e-1)
+    # LAX to FRA airport
+    assert haversine_distance(33.9416, -118.4085, 50.0336, 8.5706) == pytest.approx(
+        9139.6, rel=1e-1)
